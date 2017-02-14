8b0ca88ab54c066a1ad7fa11993c2d4225718dc1
Peter Lane <peter@peterlane.info>
2017-02-14 19:39:58 +0000

    added javaplex to CPATH in example scripts; added comments to README

--- examples/generate_pov.sh
+++ examples/generate_pov.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-CPATH="plex-viewer.jar:../lib/ext/gluegen-rt.jar:../lib/ext/jogl.jar"
+CPATH="plex-viewer.jar:../lib/javaplex.jar"
 
 java -cp $CPATH \
   edu.stanford.math.plex_viewer.PovGenerator \
